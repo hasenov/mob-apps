d42f1b0141d6efe28cbdff416049118995dfd25a
hasenov <45040471+hasenov@users.noreply.github.com>
2020-09-12 02:46:58 +0600

    сверстал секцию наши проекты, и коммер. предложения

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,16 @@
 $(function() {
 
-	// Custom JS
+	$('.slider-projects__js').slick({
+		slidesToShow: 1,
+		arrows: true,
+		prevArrow: '.slider-projects__nav--prev',
+		nextArrow: '.slider-projects__nav--next',
+		dots: true,
+		customPaging: function() {
+			return '<span class="slider-projects__dot slider__dot"></span>';
+		},
+		appendDots: '.slider-projects__dots',
+		speed: 350
+	});
 
 });
